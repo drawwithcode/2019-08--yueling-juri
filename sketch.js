5ac13ee3f5ef4923a9f01bd02f31357dcd6e0991
yueling-juri <55393310+yueling-juri@users.noreply.github.com>
2019-11-12 23:46:35 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,36 @@
-function preload(){
-  // put preload code here
+var myMap
+var myImage;
+var canvas;
+var mappa = new Mappa("Leaflet");
+var position;
+
+function preload() {
+  position = getCurrentPosition();
+  myImage = loadImage("./assets/cat.png");
+}
+
+var options = {
+  lat: 45.5061016,
+  lng: 9.1500127,
+  zoom: 14,
+  style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 }
 
 function setup() {
-  // put setup code here
+  //console.log(position);
+  canvas = createCanvas(windowWidth, windowHeight);
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas);
+
+  //createElement("p", "your latitude:" + position.latitude).style("color:grey");
+  //createElement("p", "your longitude:" + position.longitude).style("color:grey");
 }
 
 function draw() {
-  // put drawing code here
+  clear();
+
+  var myPoint = myMap.latLngToPixel(position.latitude, position.longitude);
+  image(myImage,myPoint.x,myPoint.y,myImage.width*0.3,myImage.height*0.3);
+
+
 }
